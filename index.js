758920979d4de194e060d495968344393c8fb783
Tony Glass <idttip.tg@gmail.com>
2023-10-16 12:55:25 -0700

    completed array-find-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin (record) { 
+const found = record.find((element) => element.result == "W");
+console.log (found)
+if (found) {
+return found.year
+} 
+}
